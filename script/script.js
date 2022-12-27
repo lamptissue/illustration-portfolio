43671263340dc0acbcb427514678eab920de8df4
lamptissue <ncarribine@hotmail.co.uk>
2022-12-27 13:28:32 +0000

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,12 @@
+$(function () {
+  /* 瀑布流區塊div */
+  var $container = $(".masonry");
+
+  //當圖片讀取完畢才執行
+  $container.imagesLoaded(function () {
+    //選擇瀑布流的區塊名稱
+    $container.masonry({
+      itemSelector: ".item",
+    });
+  });
+});
