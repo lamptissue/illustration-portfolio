e70de9855a05e43a430f9c0411b4119605e9ddf1
lamptissue <ncarribine@hotmail.co.uk>
2023-01-24 09:11:34 +0000

    Clean up code and add JS for date and imageload

--- script/script.js
+++ script/script.js
@@ -1,11 +1,8 @@
-$(function () {
-  /* 瀑布流區塊div */
-  var $container = $(".masonry");
-
-  //當圖片讀取完畢才執行
-  $container.imagesLoaded(function () {
-    //選擇瀑布流的區塊名稱
-    $container.masonry({
+window.addEventListener("DOMContentLoaded", function () {
+  let container = document.querySelector(".masonry");
+  let msnry;
+  imagesLoaded(container, function () {
+    msnry = new Masonry(container, {
       itemSelector: ".item",
     });
   });
